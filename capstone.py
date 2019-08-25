f9d8326a14688b88c606005ab0aaed8064350afd
arrubgn <41647890+arrubgn@users.noreply.github.com>
2019-08-26 02:17:48 +0530

    Add files via upload

---
+++ capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# THIS IS APPLIED CAPSTONE COURSE PROJECT
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
